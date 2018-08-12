89372b425cb4e8f698df96f96ec420bbad0b0ee2
Live681998 <ivanborovkov68@gmail.com>
2018-08-12 17:57:31 +0300

    user input

---
+++ Calculate.java
@@ -0,0 +1,19 @@
+public class Calculate {
+	public static void main (String[] arg) {
+		System.out.println("Calculate");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("Sum " + summ);
+		int mul = first * second;
+		System.out.println("Mul " + mul);
+		int sub = first - second;
+		System.out.println("Sub " + sub);
+		double div = first/second;
+		System.out.println("Div " + div);
+		double pow = Math.pow(first, second);
+		System.out.println("Pow " + pow);
+		//add +,-,*,/,^
+
+	}
+}
---
+++ Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+	private int result;
+	
+	//@param params
+	
+	public void add (int ... params){
+		for (Integer param : params){
+			this.result += param;
+		}
+	}
+
+	public int getResult(){
+		return this.result;
+	}
+
+	public void cleanResult(){
+		this.result = 0;
+	}
+
+}
